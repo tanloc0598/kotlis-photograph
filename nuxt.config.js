f03a45aa51992b52bfe23b9d15e8dfd5b054e65d
Nguyen Tan Loc <tnloc@outlook.com>
2022-04-14 19:35:13 +0700

    update cònig

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,9 @@ import 'dotenv/config'
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    port: process.env.PORT
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: process.env.TITTLE,
@@ -64,7 +66,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'localhost:3000/',
+    baseURL: `${process.env.BASE_DOMAIN}:${process.env.PORT}/`,
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
